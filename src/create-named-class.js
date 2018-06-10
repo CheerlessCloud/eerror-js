796e1957444c8e6c882f876e1a159346597a1572
CheerlessCloud <nnsceg@gmail.com>
2018-06-10 17:46:31 +0300

    refactor(prepare): replace new Function by hack with class in object

---
+++ src/create-named-class.js
@@ -0,0 +1,12 @@
+/**
+ * @function createNamedClass - Instantiate class constructor with passed name and base class.
+ * @exports
+ * @param {string} className - Name of created class.
+ * @param {Function} baseClass - Constructor of base class.
+ * @returns {Function} - Created class.
+ */
+export default function createNamedClass(className, baseClass) {
+  return ({
+    [className]: class extends baseClass { },
+  })[className];
+}
